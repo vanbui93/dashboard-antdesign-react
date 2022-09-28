e111f49ea60d5d1cc816b802c62360faac430621
Van Bui <vanbt@hivelab.com>
2022-09-28 09:06:03 +0700

    products => add layout & demo data

---
+++ src/pages/Products/data_demo.tsx
@@ -0,0 +1,256 @@
+import { Avatar, Button, Typography } from 'antd';
+const { Title } = Typography;
+
+// table code start
+export const columns = [
+  {
+    title: 'AUTHOR',
+    dataIndex: 'name',
+    key: 'name',
+    width: '32%'
+  },
+  {
+    title: 'FUNCTION',
+    dataIndex: 'function',
+    key: 'function'
+  },
+
+  {
+    title: 'STATUS',
+    key: 'status',
+    dataIndex: 'status'
+  },
+  {
+    title: 'EMPLOYED',
+    key: 'employed',
+    dataIndex: 'employed'
+  }
+];
+
+export const data = [
+  {
+    key: '1',
+    name: (
+      <>
+        <Avatar.Group>
+          <Avatar className="shape-avatar" shape="square" size={40}></Avatar>
+          <div className="avatar-info">
+            <Title level={5}>Michael John</Title>
+            <p>michael@mail.com</p>
+          </div>
+        </Avatar.Group>{' '}
+      </>
+    ),
+    function: (
+      <>
+        <div className="author-info">
+          <Title level={5}>Manager</Title>
+          <p>Organization</p>
+        </div>
+      </>
+    ),
+
+    status: (
+      <>
+        <Button type="primary" className="tag-primary">
+          ONLINE
+        </Button>
+      </>
+    ),
+    employed: (
+      <>
+        <div className="ant-employed">
+          <span>23/04/18</span>
+          <a href="#pablo">Edit</a>
+        </div>
+      </>
+    )
+  },
+
+  {
+    key: '2',
+    name: (
+      <>
+        <Avatar.Group>
+          <Avatar className="shape-avatar" shape="square" size={40}></Avatar>
+          <div className="avatar-info">
+            <Title level={5}>Alexa Liras</Title>
+            <p>alexa@mail.com</p>
+          </div>
+        </Avatar.Group>{' '}
+      </>
+    ),
+    function: (
+      <>
+        <div className="author-info">
+          <Title level={5}>Programator</Title>
+          <p>Developer</p>
+        </div>
+      </>
+    ),
+
+    status: (
+      <>
+        <Button className="tag-badge">ONLINE</Button>
+      </>
+    ),
+    employed: (
+      <>
+        <div className="ant-employed">
+          <span>23/12/20</span>
+          <a href="#pablo">Edit</a>
+        </div>
+      </>
+    )
+  },
+
+  {
+    key: '3',
+    name: (
+      <>
+        <Avatar.Group>
+          <Avatar className="shape-avatar" shape="square" size={40}></Avatar>
+          <div className="avatar-info">
+            <Title level={5}>Laure Perrier</Title>
+            <p>laure@mail.com</p>
+          </div>
+        </Avatar.Group>{' '}
+      </>
+    ),
+    function: (
+      <>
+        <div className="author-info">
+          <Title level={5}>Executive</Title>
+          <p>Projects</p>
+        </div>
+      </>
+    ),
+
+    status: (
+      <>
+        <Button type="primary" className="tag-primary">
+          ONLINE
+        </Button>
+      </>
+    ),
+    employed: (
+      <>
+        <div className="ant-employed">
+          <span>03/04/21</span>
+          <a href="#pablo">Edit</a>
+        </div>
+      </>
+    )
+  },
+  {
+    key: '4',
+    name: (
+      <>
+        <Avatar.Group>
+          <Avatar className="shape-avatar" shape="square" size={40}></Avatar>
+          <div className="avatar-info">
+            <Title level={5}>Miriam Eric</Title>
+            <p>miriam@mail.com</p>
+          </div>
+        </Avatar.Group>{' '}
+      </>
+    ),
+    function: (
+      <>
+        <div className="author-info">
+          <Title level={5}>Marketing</Title>
+          <p>Organization</p>
+        </div>
+      </>
+    ),
+
+    status: (
+      <>
+        <Button type="primary" className="tag-primary">
+          ONLINE
+        </Button>
+      </>
+    ),
+    employed: (
+      <>
+        <div className="ant-employed">
+          <span>03/04/21</span>
+          <a href="#pablo">Edit</a>
+        </div>
+      </>
+    )
+  },
+  {
+    key: '5',
+    name: (
+      <>
+        <Avatar.Group>
+          <Avatar className="shape-avatar" shape="square" size={40}></Avatar>
+          <div className="avatar-info">
+            <Title level={5}>Richard Gran</Title>
+            <p>richard@mail.com</p>
+          </div>
+        </Avatar.Group>{' '}
+      </>
+    ),
+    function: (
+      <>
+        <div className="author-info">
+          <Title level={5}>Manager</Title>
+          <p>Organization</p>
+        </div>
+      </>
+    ),
+
+    status: (
+      <>
+        <Button className="tag-badge">ONLINE</Button>
+      </>
+    ),
+    employed: (
+      <>
+        <div className="ant-employed">
+          <span>23/03/20</span>
+          <a href="#pablo">Edit</a>
+        </div>
+      </>
+    )
+  },
+
+  {
+    key: '6',
+    name: (
+      <>
+        <Avatar.Group>
+          <Avatar className="shape-avatar" shape="square" size={40}></Avatar>
+          <div className="avatar-info">
+            <Title level={5}>John Levi</Title>
+            <p>john@mail.com</p>
+          </div>
+        </Avatar.Group>{' '}
+      </>
+    ),
+    function: (
+      <>
+        <div className="author-info">
+          <Title level={5}>Tester</Title>
+          <p>Developer</p>
+        </div>
+      </>
+    ),
+
+    status: (
+      <>
+        <Button className="tag-badge">ONLINE</Button>
+      </>
+    ),
+    employed: (
+      <>
+        <div className="ant-employed">
+          <span>14/04/17</span>
+          <a href="#pablo">Edit</a>
+        </div>
+      </>
+    )
+  }
+];
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,8 +1,37 @@
-import { Layout } from 'antd';
+import { Card, Col, Radio, RadioChangeEvent, Row, Table } from 'antd';
 import React from 'react';
+import { columns, data } from './data_demo';
 
-const Products: React.FC = () => {
-  return <>Trang sản phẩm</>;
+const Products: React.FC = props => {
+  const onChange = (e: RadioChangeEvent): void => {
+    console.log(`radio checked:${e.target.value}`);
+  };
+  return (
+    <>
+      <div className="tabled">
+        <Row gutter={[24, 0]}>
+          <Col xs="24" xl={24}>
+            <Card
+              bordered={false}
+              className="criclebox tablespace mb-24"
+              title="Product List"
+              extra={
+                <>
+                  <Radio.Group onChange={onChange} defaultValue="a">
+                    <Radio.Button value="a">All</Radio.Button>
+                    <Radio.Button value="b">ONLINE</Radio.Button>
+                  </Radio.Group>
+                </>
+              }>
+              <div className="table-responsive">
+                <Table columns={columns} dataSource={data} pagination={false} className="ant-border-space" />
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 };
 
 export default Products;
